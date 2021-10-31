34245b0f260db051c41e05e85d95de712431011c
Mendel Dinh <mdinh6@gmail.com>
2021-10-31 07:46:31 -0700

    remove old commented out code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <table>
     <thead>
       <tr>
@@ -38,7 +36,6 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
